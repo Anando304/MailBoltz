fcbd68693496adef0b35edff4ca36f8a789d9bf6
Anando Zaman <zamana11@mcmaster.ca>
2020-05-20 00:00:44 -0400

    Need to fix the user session bug

--- WebApp/app.py
+++ WebApp/app.py
@@ -6,16 +6,10 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-@app.before_request #Use of request decorators to check if a session is active before moving onto an authenticated page
-def before_request():
-    #g is a global variable representing the session
-    g.logged_in = None
-    if 'logged_in' in session: #If a user exists or is currently logged. logged_in is the key in the session that is associated with True
-        g.user = session['logged_in']
-
 @app.route('/home', methods=['GET','POST'])
 def home():
     output = None
+
     if request.method == 'POST':
         #all buttons have the same name(submit), just diff value
         if request.form['submit'] == 'Submit':
@@ -25,8 +19,11 @@ def home():
             pass_ = request.form["password"]
             output = controller(user,pass_)
 
-            session.pop('logged_in',None) #lOGOUT of any existing session
-            session['logged_in'] = True #Create new session
+            if output == "Success":
+                session.pop('logged_in',None) #lOGOUT of any existing session
+                session['logged_in'] = True #Create new session
+                return render_template('index.html',error=output)
+
 
         elif request.form['submit'] == 'view':
             return redirect(url_for('view'))
@@ -34,10 +31,23 @@ def home():
     return render_template('index.html',error=output)
 
 
+@app.before_request #Use of request decorators to check if a session is active before moving onto an authenticated page
+def before_request():
+    #g is a global variable representing the session
+    g.logged_in = None
+    if 'logged_in' in session: #If a user exists or is currently logged. logged_in is the key in the session that is associated with True
+        g.user = session['logged_in']
+
 # View emails if available
 @app.route('/view', methods=['GET','POST'])
 def view():
     if request.method == 'GET':
+
+        # If authenticated session not yet created
+        if g.user == None:
+            return redirect(url_for('home'))
+
+        session.pop('logged_in', None)  # Pops the user value from the session dictionary essentially logging them out
         return render_template('EmailSummary.html')
           
 if __name__ == "__main__":
